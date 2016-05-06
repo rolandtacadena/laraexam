465359a44372f0b6cf1c933445ff30b0f02dfad5
vagrant <vagrant@homestead>
2016-05-06 15:27:24 +0000

    Added new migrations

---
+++ database/migrations/2016_05_06_150409_create_exams_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateExamsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('exams', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+
+            $table->integer('subject_id')->unsigned();
+            $table->foreign('subjet_id')
+                ->references('id')
+                ->on('subjects')
+                ->onDelete('cascade');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('exams');
+    }
+}

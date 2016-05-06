0ed5612d543f2fe6ed0157374eae0a187a2c78cb
rtacadena <tacadena.roland@gmail.com>
2016-05-06 15:33:36 +0000

    Updated routes file

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -11,6 +11,50 @@
 |
 */
 
+Route::auth();
+
 Route::get('/', function () {
-    return view('welcome');
+    return view('index');
 });
+
+Route::get('/home', 'HomeController@home');
+
+// dummy routes for widgetization
+Route::get('/teacher-exams', function() {
+    return view('dummy.teacher-exams');
+});
+
+Route::get('/teacher-students', function() {
+    return view('dummy.teacher-students');
+});
+
+Route::get('/teacher-reports', function() {
+    return view('dummy.teacher-reports');
+});
+
+Route::get('/teacher-settings', function() {
+    return view('dummy.teacher-settings');
+});
+
+Route::get('/teacher-create-student', function() {
+    return view('dummy.teacher-create-student');
+});
+
+Route::get('/teacher-create-subject', function() {
+    return view('dummy.teacher-create-subject');
+});
+
+Route::get('/teacher-sample-exam', function() {
+    return view('dummy.teacher-sample-exam');
+});
+
+// student
+Route::get('/student-exam', function() {
+    return view('dummy.student-exam');
+});
+
+Route::get('/student', function() {
+    return view('dummy.student');
+});
+
+//Route::get('/home', 'HomeController@index');

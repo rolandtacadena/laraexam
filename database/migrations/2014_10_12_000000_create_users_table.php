64ffbcba8f8c8a6df926b83a2745311114f03375
vagrant <vagrant@homestead>
2016-05-06 15:28:14 +0000

    Update user migration file

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,6 +15,8 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name');
+            $table->enum('role', ['student', 'teacher', 'admin'])->default('student');
+            $table->string('profile_photo');
             $table->string('email')->unique();
             $table->string('password');
             $table->rememberToken();

33a54493f2f320f50d7dc091a1750be6162bc5a7
Roland Tacadena <tacadena.roland@gmail.com>
2017-09-16 08:11:45 +0000

    Seeder Update

--- database/seeds/UserTableSeeder.php
+++ database/seeds/UserTableSeeder.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Teacher;
 use App\User;
 use Illuminate\Database\Seeder;
 
@@ -14,12 +15,18 @@ class UserTableSeeder extends Seeder
     {
         factory(User::class, 10)->create();
 
-        $user = User::find(1);
+        $user = User::first();
         $user->teacher_id = 1;
         $user->name = 'user';
         $user->email = 'user@user.com';
         $user->password = bcrypt('user');
         $user->status = true;
         $user->save();
+
+        $teacher = Teacher::first();
+        $teacher->name = 'teacher';
+        $teacher->email = 'teacher@teacher.com';
+        $teacher->password = bcrypt('teacher');
+        $teacher->save();
     }
 }

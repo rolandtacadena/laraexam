310dfa6c6c3aa6a4eb21df29c615a3b46041efdb
Roland Tacadena <tacadena.roland@gmail.com>
2017-10-06 06:00:53 +0000

    View update

--- resources/views/layouts/partials/navs/desktop-nav-left.blade.php
+++ resources/views/layouts/partials/navs/desktop-nav-left.blade.php
@@ -1,16 +1,5 @@
 <div class="top-bar-left">
     <ul class="dropdown menu" data-dropdown-menu>
         <li id="site-logo" class="menu-text"><a href="{{ route('index') }}">Laraexam</a></li>
-        {{--<li class="has-submenu">--}}
-            {{--<a href="#">Item 1</a>--}}
-            {{--<ul class="menu submenu vertical" data-submenu>--}}
-                {{--<li><a href="left_wide_11">Left wide 1</a></li>--}}
-                {{--<li><a href="left_wide_12">Left wide 2</a></li>--}}
-                {{--<li><a href="left_wide_13">Left wide 3</a></li>--}}
-            {{--</ul>--}}
-        {{--</li>--}}
-        {{--<li><a href="left_wide_11">Left wide 1</a></li>--}}
-        {{--<li><a href="left_wide_11">Left wide 1</a></li>--}}
-        {{--<li><a href="left_wide_11">Left wide 1</a></li>--}}
     </ul>
 </div>
